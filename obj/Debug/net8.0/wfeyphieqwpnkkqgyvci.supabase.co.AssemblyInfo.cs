92bd9d012f923c70f832abbc7220bceed6ea8a09
ArlanTleuberlinQA <lovitopor68@gmail.com>
2025-05-27 18:09:20 +0300

    logger fix

--- obj/Debug/net8.0/wfeyphieqwpnkkqgyvci.supabase.co.AssemblyInfo.cs
+++ obj/Debug/net8.0/wfeyphieqwpnkkqgyvci.supabase.co.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("wfeyphieqwpnkkqgyvci.supabase.co")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d94e406f325d11cfd3cd2c4b5cbfaa7cc785afe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adc3144ea7f70e573d4b238d20f0a00a2cc4a30c")]
 [assembly: System.Reflection.AssemblyProductAttribute("wfeyphieqwpnkkqgyvci.supabase.co")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wfeyphieqwpnkkqgyvci.supabase.co")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
